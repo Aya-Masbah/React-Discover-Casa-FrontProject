c8418dfac12cb584712b853b09b27ba279a3c542
Masbah Aya <123211272+AyaMasbah@users.noreply.github.com>
2023-12-25 11:50:18 +0100

    update
    maps

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
 import Search from './components/search/Search';
 import Selects from './components/selects/Selects';
+import { CssBaseline, Grid } from '@material-ui/core';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <CssBaseline />
+      <Header/>
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map/>
+        </Grid>
+      </Grid>
       <Destinations />
       <Search />
       <Selects />
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HeroStyles.css'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 import Video from '../../assets/casaVideo.mp4'
 
@@ -12,7 +13,14 @@ function Hero() {
             <div className="overlay"></div>
             <div className="content">
                 <h1>Reveal Casa's Charms</h1>
-                <button className='btn-cta'>Daylight Casa</button>
+                <form className="form">
+                    <div>
+                        <input type="text" placeholder='Search Destinations' />
+                    </div>
+                    <div>
+                        <button><AiOutlineSearch className='icon'/></button>
+                    </div>
+                </form>
             </div>
         </div>
     )
